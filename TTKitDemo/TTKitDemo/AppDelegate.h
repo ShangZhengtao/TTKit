ddff626ceb658bb82495c4b684e6f0842d36378f
shangZhengtao <shang2net@163.com>
2017-11-15 18:13:30 +0800

    Demo

---
+++ TTKitDemo/TTKitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TTKitDemo
+//
+//  Created by apple on 2017/11/15.
+//  Copyright © 2017年 shang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TTKitDemo/TTKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TTKitDemo
+//
+//  Created by apple on 2017/11/15.
+//  Copyright © 2017年 shang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
